19f1051d65544e4138623ec086b8a8e5e9a35b4b
Stivelis123 <125553694+Stivelis123@users.noreply.github.com>
2023-03-11 21:41:56 +0100

    Update forslag #1.py

--- forslag #1.py
+++ forslag #1.py
@@ -1,90 +1,117 @@
-import tkinter as tk        
+import tkinter as tk
 from tkinter import messagebox
+import sys
 
-window = tk.Tk()
-window.geometry("1000x500")
-window.title("window")
-window.configure(background="pink")
+class program(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.geometry("1500x800+100+50")
 
-def openPulswindow():
-    newwindow = tk.Tk()
-    newwindow.geometry("990x350+10+150")
-    newwindow.title("Puls")
-    newwindow.configure(background="red")
+        self.menucontainer = tk.Frame(self, bg="pink")
+        self.menucontainer.place(x=0, y=0, width=1500, height=100)
+        self.menucontainer.grid_rowconfigure(0, weight=1)
+        self.menucontainer.grid_columnconfigure(0, weight=1)
+        menu = menuframe(parent=self.menucontainer, controller=self)
+        menu.place(x=0, y=0, width=1500, height=100)
 
-    label = tk.Label(newwindow, text="Grænseværdier", font=('Arial', 12))
-    label.place(x=20, y=25, heig=20, width=90)
+        self.container = tk.Frame(self)
+        self.container.place(width=1500, height=800)
+        self.container.pack( fill="both", expand=True, padx=0,pady=100)
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
 
-    Grænse1 = tk.Entry(newwindow, background="black")
-    Grænse1.place(x=30, y=50, heig=30, width=70)
+        self.frames = {}
+        for F in (forsideframe,Graensevaerdierframe, pulsgrafframe, pulsdataframe, SpO2grafframe, SpO2dataframe):
+            page_name = F.__name__
+            frame = F(parent=self.container, controller=self)
+            self.frames[page_name] = frame
+            frame.place(x=0, y=0, width=1500, height=800)
 
-    Grænse2 = tk.Entry(newwindow, background="black")
-    Grænse2.place(x=30, y=100, heig=30, width=70)
+        self.show_frame("forsideframe")
 
-    Grænse3 = tk.Entry(newwindow, background="black")
-    Grænse3.place(x=30, y=150, heig=30, width=70)
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
 
-    Grænse4 = tk.Entry(newwindow, background="black")
-    Grænse4.place(x=30, y=200, heig=30, width=70)
+class menuframe(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.menu_frame = tk.Frame(self, bg="grey", highlightbackground="black", highlightthickness=2)
+        self.menu_frame.place(x=0, y=0, width=1500, height=100)
 
-    Grænse5 = tk.Entry(newwindow, background="black")
-    Grænse5.place(x=30, y=250, heig=30, width=70)
+        self.graenseværdier = tk.Button(self, text="Grænser", font=("Arial", 15), command=lambda: controller.show_frame("Graensevaerdierframe"))
+        self.graenseværdier.place(x=200, y=30, width=100, heigh=40)
 
-    textbox1 = tk.Text(newwindow, font=('Arial', 12))
-    textbox1.place(x=200, y=25, heig=300, width=300)
+        self.puls_graf_knap = tk.Button(self, text="Puls graf", font=("Arial", 15), command=lambda: controller.show_frame("pulsgrafframe"))
+        self.puls_graf_knap.place(x=450, y=30, width=100, heigh=40)
 
-    textbox2 = tk.Text(newwindow, font=('Arial', 12))
-    textbox2.place(x=600, y=25, heig=300, width=300)
+        self.puls_data_knap = tk.Button(self, text="Puls data", font=("Arial", 15), command=lambda: controller.show_frame("pulsdataframe"))
+        self.puls_data_knap.place(x=600, y=30, width=100, heigh=40)
 
-def openSpO2window():
-    newwindow = tk.Tk()
-    newwindow.geometry("990x350+10+150")
-    newwindow.title("SpO2")
-    newwindow.configure(background="blue")
+        self.SpO2_graf_knap = tk.Button(self, text="SpO2 graf", font=("Arial", 15), command=lambda: controller.show_frame("SpO2grafframe"))
+        self.SpO2_graf_knap.place(x=850, y=30, width=100, heigh=40)
 
-    label = tk.Label(newwindow, text="Grænseværdier", font=('Arial', 12))
-    label.place(x=20, y=25, heig=20, width=90)
+        self.SpO2_data_knap = tk.Button(self, text="SpO2 data", font=("Arial", 15), command=lambda: controller.show_frame("SpO2dataframe"))
+        self.SpO2_data_knap.place(x=1000, y=30, width=100, heigh=40)
 
-    Grænse1 = tk.Entry(newwindow, background="black")
-    Grænse1.place(x=30, y=50, heig=30, width=70)
+        self.quit_button = tk.Button(self, text="Quit", font=("Arial", 15), command=self.quit)
+        self.quit_button.place(x=0, y=0)
 
-    Grænse2 = tk.Entry(newwindow, background="black")
-    Grænse2.place(x=30, y=100, heig=30, width=70)
+    def quit(self):
+        message = tk.messagebox.askyesno("Question", "Are you sure you want to quit?")
 
-    Grænse3 = tk.Entry(newwindow, background="black")
-    Grænse3.place(x=30, y=150, heig=30, width=70)
+        if message:
+            sys.exit()
 
-    Grænse4 = tk.Entry(newwindow, background="black")
-    Grænse4.place(x=30, y=200, heig=30, width=70)
+class forsideframe(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
 
-    Grænse5 = tk.Entry(newwindow, background="black")
-    Grænse5.place(x=30, y=250, heig=30, width=70)
+        self.forside_frame = tk.Frame(self, bg="grey", highlightbackground="black", highlightthickness=2)
+        self.forside_frame.place(x=0, y=0, width=1500, height=700)
 
-    textbox1 = tk.Text(newwindow, font=('Arial', 12))
-    textbox1.place(x=200, y=25, heig=300, width=300)
+class Graensevaerdierframe(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
 
-    textbox2 = tk.Text(newwindow, font=('Arial', 12))
-    textbox2.place(x=600, y=25, heig=300, width=300)
+        self.graensevaerdier_frame = tk.Frame(self, bg="black", highlightbackground="black", highlightthickness=2)
+        self.graensevaerdier_frame.place(x=0, y=0, width=1500, height=700)
 
-def quit():
-    message = tk.messagebox.askyesno("Question","Are you sure you want to quit?")
+class pulsgrafframe(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
 
-    if message:
-        window.destroy()
+        self.puls_graf_frame = tk.Frame(self, bg="pink", highlightbackground="black", highlightthickness=2)
+        self.puls_graf_frame.place(x=0, y=0, width=1500, height=700)
 
-label = tk.Label(window, text="Patientoplysninger", font=('Arial', 15))
-label.place(x=230, y=200, heig=200, width=600)
+class pulsdataframe(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
 
-Puls_button = tk.Button(window, text="Puls", font=('Arial', 25))
-Puls_button.place(x=400, y=40, heig=50, width=100)
-Puls_button['command'] = openPulswindow
+        self.puls_data_frame = tk.Frame(self, bg="blue", highlightbackground="black", highlightthickness=2)
+        self.puls_data_frame.place(x=0, y=0, width=1500, height=700)
 
-SpO2_button = tk.Button(window, text="SpO2", font=('Arial', 25))
-SpO2_button.place(x=550, y=40, heig=50, width=100)
-SpO2_button['command'] = openSpO2window
+class SpO2grafframe(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
 
-quit_button = tk.Button(window, text="Quit")
-quit_button.place(x=930, y=470, heig=30, width=70)
-quit_button['command'] = quit
+        self.SpO2_graf_frame = tk.Frame(self, bg="green", highlightbackground="black", highlightthickness=2)
+        self.SpO2_graf_frame.place(x=0, y=0, width=1500, height=700)
 
-window.mainloop()
+class SpO2dataframe(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        self.SpO2_data_frame = tk.Frame(self, bg="yellow", highlightbackground="black", highlightthickness=2)
+        self.SpO2_data_frame.place(x=0, y=0, width=1500, height=700)
+
+if __name__ == "__main__":
+    app = program()
+    app.mainloop()
