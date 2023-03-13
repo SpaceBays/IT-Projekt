e28dece7ee0c94208872ac8ed36ce6de04b1c0e9
Stivelis123 <125553694+Stivelis123@users.noreply.github.com>
2023-03-13 19:33:43 +0100

    Update forslag #1.py

--- forslag #1.py
+++ forslag #1.py
@@ -44,25 +44,25 @@ class menuframe(tk.Frame):
         self.menu_frame = tk.Frame(self, bg="grey", highlightbackground="black", highlightthickness=2)
         self.menu_frame.place(x=0, y=0, width=1500, height=100)
 
-        self.graenseværdier = tk.Button(self, text="Grænser", font=("Arial", 15), command=lambda: controller.show_frame("Graensevaerdierframe"))
+        self.graenseværdier = tk.Button(self.menu_frame, text="Grænser", font=("Arial", 15), command=lambda: controller.show_frame("Graensevaerdierframe"))
         self.graenseværdier.place(x=200, y=30, width=100, heigh=40)
 
-        self.puls_graf_knap = tk.Button(self, text="Puls graf", font=("Arial", 15), command=lambda: controller.show_frame("pulsgrafframe"))
+        self.puls_graf_knap = tk.Button(self.menu_frame, text="Puls graf", font=("Arial", 15), command=lambda: controller.show_frame("pulsgrafframe"))
         self.puls_graf_knap.place(x=450, y=30, width=100, heigh=40)
 
-        self.puls_data_knap = tk.Button(self, text="Puls data", font=("Arial", 15), command=lambda: controller.show_frame("pulsdataframe"))
+        self.puls_data_knap = tk.Button(self.menu_frame, text="Puls data", font=("Arial", 15), command=lambda: controller.show_frame("pulsdataframe"))
         self.puls_data_knap.place(x=600, y=30, width=100, heigh=40)
 
-        self.SpO2_graf_knap = tk.Button(self, text="SpO2 graf", font=("Arial", 15), command=lambda: controller.show_frame("SpO2grafframe"))
+        self.SpO2_graf_knap = tk.Button(self.menu_frame, text="SpO2 graf", font=("Arial", 15), command=lambda: controller.show_frame("SpO2grafframe"))
         self.SpO2_graf_knap.place(x=850, y=30, width=100, heigh=40)
 
-        self.SpO2_data_knap = tk.Button(self, text="SpO2 data", font=("Arial", 15), command=lambda: controller.show_frame("SpO2dataframe"))
+        self.SpO2_data_knap = tk.Button(self.menu_frame, text="SpO2 data", font=("Arial", 15), command=lambda: controller.show_frame("SpO2dataframe"))
         self.SpO2_data_knap.place(x=1000, y=30, width=100, heigh=40)
 
-        self.forside_knap = tk.Button(self, text="Forside", font=("Arial", 15), command=lambda: controller.show_frame("forsideframe"))
+        self.forside_knap = tk.Button(self.menu_frame, text="Forside", font=("Arial", 15), command=lambda: controller.show_frame("forsideframe"))
         self.forside_knap.place(x=50, y=0)
-        
-        self.quit_button = tk.Button(self, text="Quit", font=("Arial", 15), command=self.quit)
+
+        self.quit_button = tk.Button(self.menu_frame, text="Quit", font=("Arial", 15), command=self.quit)
         self.quit_button.place(x=0, y=0)
 
     def quit(self):
