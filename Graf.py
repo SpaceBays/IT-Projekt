c16c01ff6aebc9894b00f651fe2eb1666b0d5109
SpaceBabe2001 <125360025+SpaceBabe2001@users.noreply.github.com>
2023-03-21 09:10:02 +0100

    Update Graf.py

--- Graf.py
+++ Graf.py
@@ -4,19 +4,16 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import matplotlib.animation as animation
 from matplotlib import style
-
 import tkinter as tk
 from tkinter import ttk
 import datetime
 from time import sleep
-
 
 LARGE_FONT= ("Verdana", 12)
 style.use("ggplot")
 
 f = Figure(figsize=(5,5), dpi=100)
 a = f.add_subplot(111)
-
 
 def animate(i):
     pullData = open("SpO2.txt","r").read()
@@ -27,39 +24,28 @@ def animate(i):
         if len(eachLine) > 1:
             y = eachLine
             yList.append(int(y))
-
     a.clear()
     a.plot(xList, yList)
             
-
 class SeaofBTCapp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
-        
         tk.Tk.__init__(self, *args, **kwargs)
-
         tk.Tk.wm_title(self, "Sea of BTC client")
-        
         
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand = True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-
         self.frames = {}
 
         for F in (StartPage, PageThree):
-
             frame = F(container, self)
-
             self.frames[F] = frame
-
             frame.grid(row=0, column=0, sticky="nsew")
-
         self.show_frame(StartPage)
 
     def show_frame(self, cont):
-
         frame = self.frames[cont]
         frame.tkraise()
 
@@ -70,19 +56,8 @@ class StartPage(tk.Frame):
         tk.Frame.__init__(self,parent)
         label = tk.Label(self, text="Start Page", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
-
-        # # button = ttk.Button(self, text="Visit Page 1",
-        # #                     command=lambda: controller.show_frame(PageOne))
-        # button.pack()
-
-        # button2 = ttk.Button(self, text="Visit Page 2",
-        #                     command=lambda: controller.show_frame(PageTwo))
-        # button2.pack()
-
-        button3 = ttk.Button(self, text="Graph Page",
-                            command=lambda: controller.show_frame(PageThree))
+        button3 = ttk.Button(self, text="Graph Page", command=lambda: controller.show_frame(PageThree))
         button3.pack()
-
 
 class PageThree(tk.Frame):
 
@@ -91,15 +66,12 @@ class PageThree(tk.Frame):
         label = tk.Label(self, text="Graph Page!", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
 
-        button1 = ttk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
+        button1 = ttk.Button(self, text="Back to Home", command=lambda: controller.show_frame(StartPage))
         button1.pack()
-
 
         canvas = FigureCanvasTkAgg(f, self)
         canvas.draw()
         canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-
         canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
 
